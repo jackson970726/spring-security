617bc59b14774a16274ae7b54798c29070b71c8c
jackson970726 <2773258845@qq.com>
2021-03-03 11:31:31 +0800

    SpringSecurity基于内存的授权

---
+++ security-04-authorization-memory/src/main/java/com/jackson/security/Security04AuthorizationMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security04AuthorizationMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security04AuthorizationMemoryApplication.class, args);
+    }
+
+}
