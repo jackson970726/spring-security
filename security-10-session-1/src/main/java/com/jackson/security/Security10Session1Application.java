9682357377ee1a3007ec1b9302eb4be571464d07
jackson970726 <2773258845@qq.com>
2021-03-05 16:32:01 +0800

    Spring Security 自动踢掉前一个登录用户

---
+++ security-10-session-1/src/main/java/com/jackson/security/Security10Session1Application.java
@@ -0,0 +1,13 @@
+package com.jackson.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security10Session1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security10Session1Application.class, args);
+    }
+
+}
